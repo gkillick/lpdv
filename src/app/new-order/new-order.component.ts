73e85a79a3115e8f30ca7e968e89f9b23459b849
gkillick <gkillick@gmail.com>
2020-11-18 10:03:02 -0500

    added new order form items, fields are dynamically generated based on items hash

--- src/app/new-order/new-order.component.ts
+++ src/app/new-order/new-order.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-new-order',
@@ -7,9 +9,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewOrderComponent implements OnInit {
 
-  constructor() { }
+  //form object
+  myForm: FormGroup;
+
+  loading = false;
+  success = false;
+
+  items = {
+    "breads": ["bagels", "baguettes"],
+    "baked_goods": ["cookes","chocolatines"]
+  }
+  
+  
+
+  constructor(private fb: FormBuilder, private dialogRef: MatDialogRef<NewOrderComponent>) { }
+
+  group={};
 
   ngOnInit(): void {
+
+
+
+    this.myForm = this.fb.group({
+      name: ['', Validators.required],
+    });
+    for (var key in this.items) {
+      this.items[key].forEach(element => {
+        console.log(element)
+        this.myForm.addControl(element, new FormControl('', ))
+      });
+  }
+
+
+  }
+
+  async submitHandler() {
+    this.loading = true;
+
+    const formValue = this.myForm.value;
+
+    try {
+      //use this object to create order
+      console.log(formValue)
+      this.success = true;
+      this.dialogRef.close()
+    } catch(err) {
+      console.error(err)
+    }
+
+    this.loading = false;
+    
   }
 
 }

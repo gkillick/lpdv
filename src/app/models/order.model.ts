20aa02cd0ca511607a49787bab5ddb53d1d35978
gkillick <gkillick@gmail.com>
2020-12-02 17:32:34 -0700

    fixed user id to string

--- src/app/models/order.model.ts
+++ src/app/models/order.model.ts
@@ -9,7 +9,7 @@ export class Order{
 
     constructor(
         public id: string,
-        public user_id: number,
+        public user_id: string,
         public first_name:string, 
         public last_name:string,
         public telephone: number,

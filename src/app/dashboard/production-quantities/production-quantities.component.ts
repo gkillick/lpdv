ded53abc4f644477f459e0583bcbcb7ab2db537f
Micah <killickster@gmail.com>
2021-08-29 14:01:25 -0600

    cleaned up production quantities componenent

--- src/app/dashboard/production-quantities/production-quantities.component.ts
+++ src/app/dashboard/production-quantities/production-quantities.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input, OnChanges, OnInit} from '@angular/core';
-import {MatTableDataSource} from "@angular/material/table";
-import {ItemOrdersService} from "../../services/item-orders.service";
+import {MatTableDataSource} from '@angular/material/table';
+import {ItemOrdersService} from '../../services/item-orders.service';
 
 @Component({
   selector: 'app-production-quantities',
@@ -15,7 +15,7 @@ export class ProductionQuantitiesComponent implements OnInit, OnChanges{
   @Input() dateStart: Date;
   @Input() dateEnd: Date;
 
-  displayedColumns = ["name", "type", "amount", "sliced_amount"];
+  displayedColumns = ['name', 'type', 'amount', 'sliced_amount'];
   constructor(private itemOrdersService: ItemOrdersService) { }
 
   ngOnInit(): void {
@@ -26,25 +26,14 @@ export class ProductionQuantitiesComponent implements OnInit, OnChanges{
     this.generateData();
   }
   generateData(): void{
-    const betweenDates = (date1: Date, date2: Date, date3: Date) => {
-      return date1 >= date2 && date1 <= date3;
-    };
+    const filterFunction = this.forDate ?
+      (date1: Date, date2: Date, date3: Date) => date1.toDateString() === date2.toDateString() :
+      (date1: Date, date2: Date, date3: Date) => date1 >= date2 && date1 <= date3;
 
-    const forDate = (date1: Date, date2: Date, date3: Date) => {
-      return date1.toDateString() === date2.toDateString();
-    };
-
-    if (this.forDate){
-      this.itemOrdersService.formattedItemOrdersForProductionTable(forDate, this.dateStart, this.dateEnd).subscribe(itemOrders => {
-        this.productionQuantitesData = new MatTableDataSource(itemOrders);
-        this.productionQuantitesData.filter = this.filterString;
-      });
-    }else{
-      this.itemOrdersService.formattedItemOrdersForProductionTable(betweenDates, this.dateStart, this.dateEnd).subscribe(itemOrders => {
-        this.productionQuantitesData = new MatTableDataSource(itemOrders);
-        this.productionQuantitesData.filter = this.filterString;
-      });
-    }
+    this.itemOrdersService.formattedItemOrdersForProductionTable(filterFunction, this.dateStart, this.dateEnd).subscribe(itemOrders => {
+      this.productionQuantitesData = new MatTableDataSource(itemOrders);
+      this.productionQuantitesData.filter = this.filterString;
+    });
   }
 
 }
